ff20d3ebe15b245c3358d3d03a403dd4dfe64ff5
Lucas Guzzo <lucasmguzzo@gmail.com>
2021-06-17 15:47:53 +0100

    create benchmark project

---
+++ src/TypeCaster.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeCaster.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
